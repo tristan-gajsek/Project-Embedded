bdf96309be49b09af79c3f7f3da71731ac1452b4
tristan-gajsek <tristan.gajsek@student.um.si>
2025-01-11 09:44:36 +0100

    Change parsing code

--- graph/src/data.rs
+++ graph/src/data.rs
@@ -1,12 +1,8 @@
 use std::{io, sync::Arc, time::Duration};
 
 use crate::{cli::Cli, graph::Graph};
-use anyhow::{Ok, Result};
+use anyhow::{bail, Result};
 use colored::Colorize;
-use nom::{
-    character::complete::multispace0, combinator::map_res, number::complete::double,
-    sequence::tuple, IResult,
-};
 
 #[derive(Debug, PartialEq)]
 pub struct NoiseData {
@@ -24,23 +20,23 @@ impl NoiseData {
         }
     }
 
-    fn parse(input: &str) -> IResult<&str, Self> {
-        map_res(
-            tuple((
-                multispace0,
-                double,
-                multispace0,
-                double,
-                multispace0,
-                double,
-                multispace0,
-            )),
-            |(_, lat, _, long, _, dec, _)| Ok(Self::new(lat, long, dec)),
-        )(input)
+    fn parse(input: &str) -> Result<Self> {
+        let nums = input
+            .split_whitespace()
+            .map(|s| s.parse().map_err(anyhow::Error::from))
+            .collect::<Result<Vec<_>>>()?;
+        if nums.len() != 3 {
+            bail!("Couldn't parse noise data");
+        }
+        Ok(NoiseData {
+            latitude: nums[0],
+            longitude: nums[1],
+            decibels: nums[2],
+        })
     }
 }
 
-pub fn read_serial_port(args: &Cli, graph: Arc<Graph>) -> Result<()> {
+pub fn read_serial_port(args: Cli, graph: Arc<Graph>) -> Result<()> {
     let port = serialport::new(args.path.as_ref(), args.baud_rate)
         .timeout(
             args.timeout
@@ -61,9 +57,9 @@ pub fn read_input(graph: Arc<Graph>) -> Result<()> {
     loop {
         let mut input = "".to_string();
         io::stdin().read_line(&mut input)?;
-        match NoiseData::parse(&input) {
-            IResult::Ok((_, data)) => graph.push(data)?,
-            IResult::Err(e) => eprintln!("{} {e}", "error:".red()),
+        match NoiseData::parse(input.trim()) {
+            Ok(data) => graph.push(data)?,
+            Err(e) => eprintln!("{} {e}", "error".red()),
         }
     }
 }
@@ -76,8 +72,8 @@ mod tests {
     fn data_parsing() {
         assert_eq!(
             NoiseData::new(1.0, 2.0, 3.0),
-            NoiseData::parse(" 1 2 3 ").expect("Couldn't parse data").1,
+            NoiseData::parse("1 2 3").expect("Couldn't parse data"),
         );
-        NoiseData::parse("a b c").expect_err("Successfully parsed invalid data");
+        matches!(NoiseData::parse(" 1 2 3 "), Err(_));
     }
 }
